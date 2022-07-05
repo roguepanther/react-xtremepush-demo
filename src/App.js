1ae12577c8e9342b3fc2a8ca2907d5decfdb5c51
Filip Napiorkowski <roguepanthernoob@gmail.com>
2022-07-05 13:01:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> Xtremepush React Demo </h1>
+      <p> This is a sandbox for ReactJS with Xtremepush </p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
